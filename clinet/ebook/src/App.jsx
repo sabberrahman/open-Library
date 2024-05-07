da6a85f11a62e08288a60e19f225ecbb1079cad9
sabberrahman <sabbersr1@gmail.com>
2024-05-07 10:32:04 +0600

    daisyUI n NavBar

--- clinet/ebook/src/App.jsx
+++ clinet/ebook/src/App.jsx
@@ -2,13 +2,14 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Navbar from './componets/Navbar'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1>sabby</h1>
+      <Navbar/>
     </>
   )
 }
