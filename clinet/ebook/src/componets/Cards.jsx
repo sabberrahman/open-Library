0e82c80de050c5d845a6bd3568febed21daedbe5
sabberrahman <sabbersr1@gmail.com>
2024-05-08 23:06:30 +0600

    finished home page

--- clinet/ebook/src/componets/Cards.jsx
+++ clinet/ebook/src/componets/Cards.jsx
@@ -4,20 +4,24 @@ const Cards = ({item}) => {
     console.log(item);
     return (
         <>
-        <div className="card w-96 bg-base-100 shadow-xl mx-2">
-  <figure><img src={item.image} alt="Shoes" /></figure>
-  <div className="card-body">
-    <h2 className="card-title">
-      {item.name} 
-      <div className="badge badge-secondary">{item.catagory}</div>
-    </h2>
-    <p>{item.title}</p>
-    <div className="card-actions justify-between">
-      <div className="badge badge-outline">{item.price} $</div> 
-      <div className="badge badge-outline hover:bg-blue-400 duration-30 hover:text-white">Read Now!</div>
-    </div>
-  </div>
-</div>
+        <div className='mt-2 my-3 p-3 '>
+            <div className="card w-92 bg-base-100 shadow-xl mx-2">
+              <figure><img src={item.image} alt="Shoes" /></figure>
+              <div className="card-body">
+                <h2 className="card-title">
+                  {item.name} 
+                  <div className="badge badge-secondary">{item.catagory}</div>
+                </h2>
+                <p>{item.title}</p>
+                <div className="card-actions justify-between">
+                  <div className="badge badge-outline">{item.price} $</div> 
+                  <div className=" badge badge-outline hover:bg-blue-400 duration-30 hover:text-white">Read Now!</div>
+                </div>
+              </div>
+            </div>
+
+        </div>
+        
             
         </>
     );
