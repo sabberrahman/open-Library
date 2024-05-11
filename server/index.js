44abb02c58c437eff4fa050dffe773f1fb3866b2
sabberrahman <sabbersr1@gmail.com>
2024-05-11 20:22:26 +0600

    connect to mongodb

--- server/index.js
+++ server/index.js
@@ -1,12 +1,30 @@
 
-const express = require('express')
+import express from 'express'
+import dotenv from 'dotenv'
 const app = express()
-const port = 3000
+import mongoose from 'mongoose'
+
+dotenv.config();
+const port =    process.env.PORT || 4000 ; 
+
+const uri = "mongodb+srv://sabber:wPQqZ2KLGcT8AjOM@cluster0.0zldvew.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+async function connectToMongo() {
+  try {
+    await mongoose.connect(uri);
+    console.log("Connected to MongoDB successfully!");
+  } catch (error) {
+    console.error("Error connecting to MongoDB:", error);
+   
+  }
+}
+
+connectToMongo(); 
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`server is listening on port ${port}`)
 })
