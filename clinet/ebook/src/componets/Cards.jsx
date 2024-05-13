a151a920eb7cc51a8ee682233287885a38e53e66
sabberrahman <sabbersr1@gmail.com>
2024-05-13 10:57:16 +0600

    about me . github . and darkmode bug fixed . updated json

--- clinet/ebook/src/componets/Cards.jsx
+++ clinet/ebook/src/componets/Cards.jsx
@@ -6,7 +6,7 @@ const Cards = ({item}) => {
         <>
         <div className='mt-2 my-3 p-3 '>
             <div className="card w-92 bg-base-100 shadow-xl mx-2 hover:scale-105 duration-300 dark:bg-gray-100 dark:text-gray-950 dark:border">
-              <figure><img src={item.image} alt="Shoes" /></figure>
+              <figure><img src={item.image} className='o object-cover'alt="book" /></figure>
               <div className="card-body">
                 <h2 className="card-title">
                   {item.name} 
